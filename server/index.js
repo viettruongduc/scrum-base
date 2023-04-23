39f6f44cdbeec70deb9cbc5bc270c93c618d0ce1
VietTruongDuc <vietdt301@gmail.com>
2023-04-23 10:02:22 +0700

    remove file

--- server/index.js
+++ server/index.js
@@ -1,58 +1,27 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const cors = require('cors');
-const PORT = 4000;
-require('dotenv').config();
+const express = require('express')
+const path = require('path')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const route = require('./src/routes')
+require('dotenv').config()
 
-const Task = require('./src/app/models/Task');
+// set port number
+const PORT = 4000
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
-app.use(express.json());
+app.use(express.json())
 
+mongoose.createConnection(process.env.ATLAS_URI)
+
+route(app)
 
 app.listen(PORT, () => {
 	console.log(`Example app listening on PORT ${PORT}`)
 })
 
-app.get("/", async (req, res) => {
-	try {
-		const tasks = await Task.find();
-		res.send(tasks);
-	} catch (err) {
-		console.log(err);
-	}
-});
-
-app.post("/", async (req, res) => {
-	const fetchID = () => Math.random().toString(36).substring(2, 10);
-	const data = req.body.data
-	const index = fetchID()
-	let result = await Task.create({
-		id: index,
-		title: data.status,
-		items: {
-			id: index,
-			title: data.title,
-			link: data.link,
-			label: data.label,
-			device: data.device,
-			deadline: data.deadline,
-		},
-	})
-	res.send(result).status(204);
-});
-
-app.patch("/", async (req, res) => {
-	try {
-		const { body } = req;
-		await Task.updateOne({ id: body.id }, { title: body.title }).exec()
-	} catch (err) {
-		console.log(err);
-	}
-});
